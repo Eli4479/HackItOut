bdb07c19fddb74cdd01e3b18f543f7df5a717809
Eli4479 <patel.aryan.eee21@itbhu.ac.in>
2023-03-17 19:51:37 +0530

    added react router and routes

---
+++ synergy/frontend/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Log from "./pages/login";
+import React from 'react';
+import Registration from "./pages/register";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Registration />} />
+            <Route path="/login" element={<Log />} />
+            {/* <Log /> */}
+            {/* <Registration /> */}
+          </Routes>
+        </Router>
+      </>
+    </div>
+  );
+}
+
+export default App;
