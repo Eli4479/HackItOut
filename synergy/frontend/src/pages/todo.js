05848e0bcff819238559c7cd1c2c57fa90b01db7
Eli4479 <patel.aryan.eee21@itbhu.ac.in>
2023-03-19 08:54:53 +0530

    fixed

--- synergy/frontend/src/pages/todo.js
+++ synergy/frontend/src/pages/todo.js
@@ -10,32 +10,6 @@ function Todo() {
   const [todos, setTodos] = useState([]);
   useEffect(() => {
 
-    const get_user = async (id) => {
-      try {
-        let response = await fetch(`http://localhost:3000/api/user/${id.assigned_to}`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
-        let data = await response.text();
-        let profile = JSON.parse(data);
-        id.assigned_to = profile.username;
-
-        // for every todo in todos, if todo.id === id.id, then todo.assigned_to = profile.username
-        for (let todo of todos) {
-          if (todo.id === id.id) {
-            todo.assigned_to = profile.username;
-            setTodos(todos);
-          }
-        }
-      }
-      catch (err) {
-        console.log(err);
-      }
-    }
-
-
     const get_team = async () => {
       const team_id = localStorage.getItem("team_id");
       let response = await fetch(`http://localhost:3000/api/user/teams/${team_id}`, {
@@ -60,31 +34,12 @@ function Todo() {
         });
         let data = await response.text();
         let todo_info = JSON.parse(data);
-
-        // for todo_info.assigned_to, get the profile name
-        let response2 = await fetch(`http://localhost:3000/api/user/${todo_info.assigned_to}`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json"
-          }
-        });
-        let data2 = await response2.text();
-        let profile = JSON.parse(data2);
-        todo_info.assigned_to = profile.username;
-
-
         the_todos.push(todo_info);
       }
       console.log(the_todos);
       setTodos(the_todos);
-      return the_todos
     }
-    get_team().then((the_todos) => {
-      the_todos.map((todo) => {
-        get_user(todo);
-      })
-    })
-
+    get_team();
   }, [])
   return (
     <div className='flex '>
